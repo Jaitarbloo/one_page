b28612d9c4a21300effa465832cc1a56da0d4027
lagun <lagun@example.com>
2026-01-01 06:38:53 +0100

    Añadiendo mejoras y componentes

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="One_page",
-    admin_dash=False,
     api_url="https://one-zrwg.onrender.com:8000",
     plugins=[
         rx.plugins.SitemapPlugin(),
