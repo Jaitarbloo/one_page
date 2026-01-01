9de54310495e3b76e8d0e8e79ec9850ef5f6ea39
lagun <lagun@example.com>
2026-01-01 06:55:24 +0100

    Añadiendo mejoras y componentes

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,12 @@ import reflex as rx
 config = rx.Config(
     app_name="One_page",
     api_url="https://one-zrwg.onrender.com:8000",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "http://https://one-page-exqm.vercel.app/",
+        "https://www.jaitarbloo.es/"
+        ],
+    
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
